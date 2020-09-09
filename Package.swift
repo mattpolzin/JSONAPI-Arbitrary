b724e1e7d483401a737ad636ac1d6da4ab662406
Mathew Polzin <matt.polzin@gmail.com>
2020-09-09 07:19:35 -0700

    Update Package.swift
    Switch to Swift Package Manager dependency URLs that contain the .git extension. This is not required by SwiftPM, but if a project does not consistently use the .git extension on dependency URLs (or not) then SwiftPM can get into trouble. The majority of open source projects use or suggest using the .git extension, so this project will standardize in that direction as well.

--- Package.swift
+++ Package.swift
@@ -11,8 +11,8 @@ let package = Package(
             targets: ["JSONAPIArbitrary"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/typelift/SwiftCheck", .upToNextMinor(from: "0.12.0")),
-        .package(url: "https://github.com/mattpolzin/JSONAPI", from: "4.0.0"),
+        .package(url: "https://github.com/typelift/SwiftCheck.git", .upToNextMinor(from: "0.12.0")),
+        .package(url: "https://github.com/mattpolzin/JSONAPI.git", from: "4.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
